866f601d54d688163a3865d7403180989a2a2d2b
yara-almeida <yaraalmeida.dev@gmail.com>
2024-08-21 21:15:08 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import airplaneSvg from "../assets/airplane.svg";
+import logoSvg from "../assets/logo.svg";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="overflow-hidden">
+            <section
+                id="hero"
+                className="w-full h-screen bg-white flex flex-col mx-auto max-w-screen-xl p-8"
+            >
+                <nav className="items-center justify-between w-full flex">
+                    <Link href="/">
+                        <Image
+                            className="w-14 h-14"
+                            src={logoSvg}
+                            alt="Logo da página"
+                        />
+                    </Link>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                    <div className="flex gap-8 items-center">
+                        <Link
+                            href="#hero"
+                            className="font-medium hover:text-fuchsia-500 transition-colors text-stone-700 rounded-full"
+                        >
+                            Preço
+                        </Link>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+                        <Link
+                            href="#hero"
+                            className="font-medium hover:text-fuchsia-500 transition-colors text-stone-700 rounded-full"
+                        >
+                            Sobre
+                        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                        <Link
+                            href="#hero"
+                            className="font-medium hover:text-fuchsia-500 transition-colors text-stone-700 rounded-full"
+                        >
+                            Blog
+                        </Link>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                        <Link
+                            href="#hero"
+                            className="font-medium hover:bg-fuchsia-700 transition-colors py-3 px-5 text-white bg-fuchsia-500 rounded-full"
+                        >
+                            Começar
+                        </Link>
+                    </div>
+                </nav>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                <div className="items-center justify-between mt-8 w-full flex">
+                    <div>
+                        <h1 className="text-left text-7xl font-black text-stone-800">
+                            Planeje sua viagem com o
+                            <span className="text-fuchsia-500">
+                                {" "}
+                                AirPlanner
+                            </span>
+                            .
+                        </h1>
+                        <p className="text-lg mt-8 font-bold text-stone-700">
+                            Nunca foi tão fácil planejar uma viagem.
+                            <br />
+                            Faça tudo de forma rápida e sem dor de cabeça.
+                        </p>
+                    </div>
+
+                    <Image
+                        className="w-full max-w-2xl"
+                        src={airplaneSvg}
+                        alt="Desenho de um avião sobrevoando a Terra"
+                    />
+                </div>
+                <Link
+                    href="#hero"
+                    className="font-medium hover:bg-fuchsia-700 transition-colors py-3 px-5 text-white bg-fuchsia-500 rounded-full w-fit mx-auto -mt-20"
+                >
+                    Começar
+                </Link>
+            </section>
+        </main>
+    );
 }
+
+// font-medium hover:bg-fuchsia-700 transition-colors py-3 px-5 text-white bg-fuchsia-500 rounded-full w-fit mx-auto
